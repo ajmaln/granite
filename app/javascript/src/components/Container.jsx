690386c8bf81a60e15cfb7e1aa195d22bc74b3f3
Ajmal Noushad <ajufhn@gmail.com>
2021-09-06 15:12:26 +0530

    Added reusable components

---
+++ app/javascript/src/components/Container.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import PropTypes from "prop-types";
+
+import NavBar from "components/NavBar";
+
+const Container = ({ children }) => {
+  return (
+    <>
+      <NavBar />
+      <div className="px-4 py-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div className="max-w-3xl mx-auto">{children}</div>
+      </div>
+    </>
+  );
+};
+
+Container.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
+export default Container;
